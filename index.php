7045c5e5b685963c98f5e259d2b231b3ac705e7e
Dhanashree <dhanashree.issc@gmail.com>
2017-03-03 19:22:24 +0530

    image

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
+	<background-image: url('Hidden Beach Mac Travel Beach Guide Tours935.jpg')/>
 	<table>
 		<tr>
 			<td style='width: 30%;'>
 				<img class = 'newappIcon' src='images/tour_guide.png'>
-			</td>
+		        </td>
 			<td>
 				<h1 id = "message"><?php echo "Think Before You Talk!"; ?></h1>
 				<p class='description'></p> Talk To The Hand <span class="blue">WatsonTourGuideApplication</span>.
